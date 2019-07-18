f181f9bdd1f5fcbb9264b773ce120ff9b405c15c
Allabergen <allromis@gmail.com>
2019-07-18 23:34:49 +0600

    change transaction listview style with ListTile

--- lib/widgets/transaction_list.dart
+++ lib/widgets/transaction_list.dart
@@ -10,7 +10,7 @@ class TransactionList extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      height: 400,
+      height: 390,
       color: Theme.of(context).backgroundColor,
       child: transactions.isEmpty
           ? Column(
@@ -29,48 +29,25 @@ class TransactionList extends StatelessWidget {
           : ListView.builder(
               itemBuilder: (context, index) {
                 return Card(
-                  elevation: 2.0,
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: <Widget>[
-                      Container(
-                        padding: EdgeInsets.all(8.0),
-                        margin: EdgeInsets.symmetric(
-                            horizontal: 16.0, vertical: 8.0),
-                        decoration: BoxDecoration(
-                            border:
-                                Border.all(color: Colors.blueGrey, width: 2.0)),
-                        child: Text(
-                          '\$${transactions[index].amount.toStringAsFixed(2)}',
-                          style: TextStyle(
-                            color: Colors.blueGrey,
-                          ),
+                  elevation: 4.0,
+                  margin: EdgeInsets.symmetric(vertical: 8.0, horizontal: 4.0),
+                  child: ListTile(
+                    leading: CircleAvatar(
+                      radius: 30.0,
+                      child: Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: FittedBox(
+                          child: Text('\$${transactions[index].amount}'),
                         ),
                       ),
-                      Container(
-                        margin: EdgeInsets.only(right: 16.0),
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.end,
-                          children: <Widget>[
-                            Text(
-                              transactions[index].title,
-                              style: TextStyle(
-                                fontSize: 16.0,
-                                color: Colors.black87,
-                              ),
-                            ),
-                            Text(
-                              DateFormat('yMMMd')
-                                  .format(transactions[index].date),
-                              style: TextStyle(
-                                fontSize: 10.0,
-                                color: Colors.grey,
-                              ),
-                            ),
-                          ],
-                        ),
-                      )
-                    ],
+                    ),
+                    title: Text(
+                      transactions[index].title,
+                    ),
+                    subtitle: Text(
+                      DateFormat.yMMMd().format(transactions[index].date),
+                      style: TextStyle(fontSize: 12.0),
+                    ),
                   ),
                 );
               },
